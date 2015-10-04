8e209225c03e82bfa193e07cc55c8b204b66577d
Sam Nicholson <snicholson@workanyware.co.uk>
2015-10-04 20:28:45 +0100

    Added wrapper cache class and tests

---
+++ src/Cache.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace SNicholson\SlimFileCache;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
+/**
+ * Class Cache
+ * @package SNicholson\SlimFileCache
+ */
+class Cache
+{
+    const HOUR = 3600;
+    const DAY = 86400;
+    const WEEK = 604800;
+    const NEVER = -1;
+
+    private $directory;
+    private $app;
+
+    public function __construct(App $app, $cacheDirectory = __DIR__ . '/../cache/')
+    {
+        $this->directory = $cacheDirectory;
+        $this->fileHandler = new FileHandler($cacheDirectory);
+        $this->app = $app;
+    }
+
+    /**
+     * Slim required __invoke magic method for middleware
+     *
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
+    {
+        $requestPath = $request->getUri()->getPath();
+        /** @var File $cache */
+        $cache = $this->get($requestPath);
+        if ($cache instanceof File) {
+            return $response->getBody()->write($cache->getContent());
+        }
+        $response = $next($request, $response);
+        return $response;
+    }
+
+    /**
+     * Removes all cache entries in the given directory
+     */
+    public function flush()
+    {
+        $this->fileHandler->deleteAll();
+    }
+
+    /**
+     * Returns the cached string for the given cacheKey
+     *
+     * @param $cacheKey
+     *
+     * @return bool|File
+     */
+    public function get($cacheKey)
+    {
+        return $this->fileHandler->read($cacheKey);
+    }
+
+    /**
+     * Adds a cache entry with a given key, content and for a set amount of time
+     * The time by default for the cache is an hour
+     * @param     $cacheKey
+     * @param     $content
+     * @param int $expires
+     */
+    public function add($cacheKey, $content, $expires = Cache::HOUR)
+    {
+        $file = File::create();
+        $file->setContent($content);
+        $file->setRoute($cacheKey);
+        if ($expires > 0) {
+            $file->setExpires(time() + $expires);
+        } else {
+            $file->setExpires($expires);
+        }
+        $this->fileHandler->write($file);
+    }
+
+    /**
+     * Removes the cache entry for the given key
+     * @param $cacheKey
+     */
+    public function remove($cacheKey)
+    {
+        $this->fileHandler->delete($cacheKey);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDirectory()
+    {
+        return $this->directory;
+    }
+}
---
+++ tests/SlimFileCache/CacheTest.php
@@ -0,0 +1,118 @@
+<?php
+
+use Slim\App;
+use Slim\Container;
+use Slim\Http\Environment;
+
+class CacheTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test Test cache saves request file in specified directory
+     */
+    public function testCacheSavesRequestFileInSpecifiedDirectory()
+    {
+        $slim = $this->getSampleSlimApp('/foo', 'slim-cache.slim');
+
+        // Add file cache middleware
+        $slim->add($cache = new \SNicholson\SlimFileCache\Cache($slim));
+
+        //Run the app
+        $slim->run(true);
+
+        $cache->add('/foo.cache', 'something');
+
+        //Check that the file was created in the expected place
+        $expectedCacheFile = $cache->getDirectory() . sha1('/foo.cache');
+        $this->assertTrue(file_exists($expectedCacheFile));
+        unlink($expectedCacheFile);
+    }
+
+    /**
+     * @test Test cache loads request file when present in specified directory
+     */
+    public function testCacheLoadsRequestFileWhenPresentInSpecifiedDirectory()
+    {
+        $slim = $this->getSampleSlimApp('/foo', 'slim-cache.slim');
+        // Add file cache middleware
+        $slim->add($cache = new \SNicholson\SlimFileCache\Cache($slim));
+
+        //Manually clear the cache of any entries
+        $cache->flush();
+
+        //Manually create the cache file we expect
+        $cache->add('/foo', 'foo-bar');
+
+        $slim->run(true);
+
+        //Check that the file was created in the expected place
+        $this->assertEquals('foo-bar', $slim->response->getBody()->__toString());
+
+        $cache->remove('/foo');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRemoveWorks()
+    {
+        $slim = $this->getSampleSlimApp('/foo', 'slim-cache.slim');
+        // Add file cache middleware
+        $cache = new \SNicholson\SlimFileCache\Cache($slim);
+        $cache->add('/foo', 'cached');
+        $slim->add($cache);
+
+        $cache->remove('/foo');
+
+        $slim->run(true);
+
+        //Check that the file was created in the expected place
+        $this->assertEquals('foo', $slim->response->getBody()->__toString());
+
+        $cache->remove('/foo');
+    }
+
+    public function testPurgeRemovesAllCacheEntries()
+    {
+        $slim = $this->getSampleSlimApp('/foo', 'slim-cache.slim');
+        // Add file cache middleware
+        $cache = new \SNicholson\SlimFileCache\Cache($slim);
+        $cache->add('/foo', 'cached');
+        $cache->add('/foo2', 'cached');
+        $cache->add('/foo3', 'cached');
+        $cache->flush();
+        $this->assertFalse($cache->get('/foo'));
+        $this->assertFalse($cache->get('/foo2'));
+        $this->assertFalse($cache->get('/foo3'));
+    }
+
+    /**
+     * @param        $uri
+     * @param        $serverName
+     * @param string $requestMethod
+     *
+     * @return App
+     */
+    private function getSampleSlimApp($uri, $serverName, $requestMethod = 'GET')
+    {
+        $container = new Container(
+            [
+                'environment' => Environment::mock(
+                    [
+                        'REQUEST_METHOD' => $requestMethod,
+                        'REQUEST_URI'   => $uri,
+                        'SERVER_NAME'    => $serverName,
+                    ]
+                )
+            ]
+        );
+        $slim = new App($container);
+        $slim->get('/foo',function(\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+            return 'foo';
+        });
+        $slim->get('/bar',function(\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+            return 'bar';
+        });
+        return $slim;
+    }
+}
